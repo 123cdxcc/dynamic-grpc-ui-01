fc1842b06066ab44c6c7405069b92ea06b2d3bd8
hongfuz <hongfuz@veigit.com>
2024-06-17 17:46:41 +0800

    feature: MetadataEdit修改为List实现

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -1,5 +1,5 @@
-import type { TableColumnsType, TabsProps } from 'antd'
-import { Table, Tabs } from 'antd'
+import type { TabsProps } from 'antd'
+import { List, Tabs } from 'antd'
 import TextArea from 'antd/es/input/TextArea'
 import type { ReactElement } from 'react'
 
@@ -8,80 +8,12 @@ function MessageEdit(): ReactElement {
 }
 
 function MetadataEdit(): ReactElement {
-	interface DataType {
-		key: React.Key
-		name: string
-		age: number
-		address: string
-	}
-
-	const columns: TableColumnsType<DataType> = [
-		{
-			title: 'Name',
-			dataIndex: 'name',
-			render: (text: string) => <div>{text}</div>
-		},
-		{
-			title: 'Age',
-			dataIndex: 'age'
-		},
-		{
-			title: 'Address',
-			dataIndex: 'address'
-		}
-	]
-
-	const data: DataType[] = [
-		{
-			key: '1',
-			name: 'John Brown',
-			age: 32,
-			address: 'New York No. 1 Lake Park'
-		},
-		{
-			key: '2',
-			name: 'Jim Green',
-			age: 42,
-			address: 'London No. 1 Lake Park'
-		},
-		{
-			key: '3',
-			name: 'Joe Black',
-			age: 32,
-			address: 'Sydney No. 1 Lake Park'
-		},
-		{
-			key: '4',
-			name: 'Disabled User',
-			age: 99,
-			address: 'Sydney No. 1 Lake Park'
-		}
-	]
-	const rowSelection = {
-		onChange: (
-			selectedRowKeys: React.Key[],
-			selectedRows: DataType[]
-		): void => {
-			console.log(
-				`selectedRowKeys: ${selectedRowKeys.toString()}`,
-				'selectedRows:',
-				selectedRows
-			)
-		},
-		getCheckboxProps: (record: DataType) => ({
-			disabled: record.name === 'Disabled User', // Column configuration not to be checked
-			name: record.name
-		})
-	}
+	const data = ['a', 'b', 'c']
 	return (
-		<Table
-			rowSelection={{
-				type: 'checkbox',
-				...rowSelection
-			}}
-			pagination={false}
-			columns={columns}
+		<List
+			bordered
 			dataSource={data}
+			renderItem={item => <List.Item>{item}</List.Item>}
 		/>
 	)
 }
