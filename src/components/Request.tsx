1a8916800fc653200a0c90db47efac86c8ee41ad
hongfuz <hongfuz@hongfuzdeMacBook-Pro.local>
2024-06-17 23:38:06 +0800

    feature: 完善metadata样式

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -1,19 +1,82 @@
 import type { TabsProps } from 'antd'
-import { List, Tabs } from 'antd'
+import { Button, Input, List, Space, Tabs } from 'antd'
 import TextArea from 'antd/es/input/TextArea'
-import type { ReactElement } from 'react'
+import { useState, type ReactElement } from 'react'
 
 function MessageEdit(): ReactElement {
 	return <TextArea style={{ height: '100%', resize: 'none' }} />
 }
 
 function MetadataEdit(): ReactElement {
-	const data = ['a', 'b', 'c']
+	interface DataItem {
+		key: string
+		value: string
+		keyEditable: boolean
+		valueEditable: boolean
+	}
+	const [data, setData] = useState<DataItem[]>([
+		{
+			key: 'key1',
+			value: 'value',
+			keyEditable: false,
+			valueEditable: false
+		},
+		{
+			key: 'key2',
+			value: 'value',
+			keyEditable: false,
+			valueEditable: false
+		}
+	])
+	const onAddItem = () => {
+		setData([
+			...data,
+			{ key: '', value: '', keyEditable: true, valueEditable: true }
+		])
+	}
+	const onRemoveItem = (index: number) => {
+		const newData: DataItem[] = []
+		for (const [i, datum] of data.entries()) {
+			if (i !== index) {
+				newData.push(datum)
+			}
+		}
+		console.log(newData.map(item => item.key).toString(), index)
+		setData(newData)
+	}
 	return (
 		<List
+			className='overflow-auto'
 			bordered
 			dataSource={data}
-			renderItem={item => <List.Item>{item}</List.Item>}
+			header={
+				<div className='flex'>
+					<div>metadata list</div>
+					<div className='flex flex-1 flex-row-reverse'>
+						<Button onClick={onAddItem}>新增</Button>
+					</div>
+				</div>
+			}
+			renderItem={(item, index) => (
+				<List.Item key={index}>
+					<Space direction='vertical' className='w-full'>
+						<Space.Compact block>
+							<Input
+								defaultValue={item.key}
+								className='flex-1'
+								placeholder='key'
+							/>
+							<Input
+								id='a'
+								defaultValue={item.value}
+								className='flex-1'
+								placeholder='value'
+							/>
+							<Button onClick={() => onRemoveItem(index)}>删除</Button>
+						</Space.Compact>
+					</Space>
+				</List.Item>
+			)}
 		/>
 	)
 }
@@ -32,5 +95,5 @@ const items: TabsProps['items'] = [
 ]
 
 export default function Request(): ReactElement {
-	return <Tabs items={items} />
+	return <Tabs className='h-full' items={items} />
 }
